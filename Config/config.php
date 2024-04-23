b72a4aeca1c48d9334640f9697e04dad77ddb6d3
master7228 <46382975+master7228@users.noreply.github.com>
2024-04-23 13:12:10 -0500

    Update config.php

--- Config/config.php
+++ Config/config.php
@@ -3,6 +3,5 @@
     const ROOT = '';
     const VIEWS = 'Views/';
     define('DTF', 'Default/');
-    define('URL', 'https://logistica.comerciovirtualseguro.com/');
-    //define('URL', 'http://localhost/urban/');
+ 
  ?>
